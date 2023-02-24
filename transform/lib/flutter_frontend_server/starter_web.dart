3b52bf885431d2a554e5ec02bc4e007a00f1fd42
skyerpfli <skylerpfli@tencent.com>
2023-02-24 15:39:16 +0800

    添加web插桩方式

---
+++ transform/lib/flutter_frontend_server/starter_web.dart
@@ -0,0 +1,22 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// @dart=2.8
+library frontend_server;
+
+import 'package:vm/target/flutter.dart';
+import 'dart:io';
+import '../hook/type_transformer.dart';
+import 'package:compiler/src/dart2js.dart' as dart2js;
+
+final TypeTransformer typeTransformer = TypeTransformer();
+
+Future<void> main(List<String> args) async {
+  ///在FlutterTarget中加入Transformer
+  if (!FlutterTarget.flutterProgramTransformers.contains(typeTransformer)) {
+    FlutterTarget.flutterProgramTransformers.add(typeTransformer);
+  }
+
+  dart2js.main(args);
+}
